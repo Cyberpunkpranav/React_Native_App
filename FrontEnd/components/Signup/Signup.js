04e85bba620a5edaede78c8a6eaeb2df3b90c7a7
Pranav Sharma <88233021+Cyberpunkpranav@users.noreply.github.com>
2023-04-08 17:24:48 +0530

    8 Apr 5pm

--- FrontEnd/components/Signup/Signup.js
+++ FrontEnd/components/Signup/Signup.js
@@ -1,9 +1,9 @@
-import { Text, View, SafeAreaView, TextInput, Pressable, Button } from 'react-native';
+import { Text, View, SafeAreaView, TextInput, Pressable, Button, TouchableOpacity, Image } from 'react-native';
 import React, { Component, useEffect, useRef, useState } from 'react'
 import signup from '../../styles/SignupStyle'
-import {safearea,bootstrap } from '../../constants/Bootstrap';
+import { safearea, bootstrap } from '../../constants/Bootstrap';
 import axios from 'axios'
-function Signup() {
+function Signup({ navigation }) {
     const confirmpassRef = useRef(null)
     const [firstname, setfirstname] = useState()
     const [lastname, setlastname] = useState()
@@ -16,7 +16,7 @@ function Signup() {
     async function signupfunc() {
         try {
             console.log(firstname, lastname, emailid, phone, password)
-            await axios.post('http://192.168.0.104:3001/signup', {
+            await axios.post('http://192.168.1.8:3001/signup', {
                 firstname: firstname,
                 lastname: lastname,
                 email_id: emailid,
@@ -24,8 +24,8 @@ function Signup() {
                 password: password
             }).then((response) => {
 
-                    alert(response.data.message)
-           
+                alert(response.data.message)
+
             })
         } catch (e) {
             alert(e.message)
@@ -39,7 +39,7 @@ function Signup() {
                     borderColor: 'green',
                 },
             });
-        } else if (confirmpassword !==undefined && confirmpassword.length>0 && password !== confirmpassword) {
+        } else if (confirmpassword !== undefined && confirmpassword.length > 0 && password !== confirmpassword) {
             confirmpassRef.current.setNativeProps({
                 style: {
                     borderColor: 'red',
@@ -58,23 +58,30 @@ function Signup() {
     }, [confirmpassword])
 
     return (
-        <SafeAreaView style={[safearea.AndroidSafeArea,bootstrap.container,bootstrap.bg_white]}>
-      
-              <View style={{marginTop:200,alignItems:'center'}}>
-            <Text style={bootstrap.fs_4_5}>Material Motors</Text>
-            <View style={{flexDirection:'row'}}>
-<TextInput style={[{flex:1,margin:5}, bootstrap.input, bootstrap.bg_palepurple]} onChangeText={(text) => { setfirstname(text) }} placeholder='Firstname' />
-<TextInput style={[{flex:1,margin:5}, bootstrap.input, bootstrap.bg_palepurple]} onChangeText={(text) => { setlastname(text) }} placeholder='Lastname' />
-            </View>
-<TextInput style={[bootstrap.input, bootstrap.bg_palepurple,{width:375}]} onChangeText={(text) => { setemailid(text) }} placeholder='Email ID' />
-<TextInput style={[bootstrap.input, bootstrap.bg_palepurple,{width:375}]} onChangeText={(text) => { setphone(text) }} placeholder='Phone No.' />
-<View style={{flexDirection:'row'}}>
-<TextInput style={[{flex:1,margin:5},bootstrap.input, bootstrap.bg_palepurple]} onChangeText={(text) => { setpassword(text) }} secureTextEntry={true}placeholder='Password' />
-<TextInput ref={confirmpassRef} style={[{flex:1,margin:5},bootstrap.input, bootstrap.bg_palepurple]} onChangeText={(text) => { setconfirmpassword(text) }} placeholder='Confirm Password' />
-</View>
-            <Pressable style={bootstrap.btn_darkpurple} onPress={signupfunc}>
-                <Text style={[bootstrap.text_white, bootstrap.text_semibold]}>Create Account </Text>
-            </Pressable>
+        <SafeAreaView style={[safearea.AndroidSafeArea, bootstrap.container, bootstrap.bg_white]}>
+
+            <View style={{ marginTop: 30, alignItems: 'center', marginTop: 150 }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')} style={{ flexDirection: 'row',paddingRight:20,marginTop:2 }}>
+                        <Image source={require('../../assets/images/backarrow.png')} style={{ width: 30, height: 30, alignSelf: 'center' }} />
+                        <Text style={[{ alignSelf: 'center' }, bootstrap.text_xanthous]}>Login</Text>
+                    </TouchableOpacity>
+                    <Text style={[bootstrap.fs_4_5, { flex: 3 }]}>Material Motors</Text>
+                </View>
+
+                <View style={{ flexDirection: 'row' }}>
+                    <TextInput style={[{ flex: 1, margin: 5 }, bootstrap.input, bootstrap.bg_palepurple]} onChangeText={(text) => { setfirstname(text) }} placeholder='Firstname' />
+                    <TextInput style={[{ flex: 1, margin: 5 }, bootstrap.input, bootstrap.bg_palepurple]} onChangeText={(text) => { setlastname(text) }} placeholder='Lastname' />
+                </View>
+                <TextInput style={[bootstrap.input, bootstrap.bg_palepurple, { width: 375 }]} onChangeText={(text) => { setemailid(text) }} placeholder='Email ID' />
+                <TextInput style={[bootstrap.input, bootstrap.bg_palepurple, { width: 375 }]} onChangeText={(text) => { setphone(text) }} placeholder='Phone No.' />
+                <View style={{ flexDirection: 'row' }}>
+                    <TextInput style={[{ flex: 1, margin: 5 }, bootstrap.input, bootstrap.bg_palepurple]} onChangeText={(text) => { setpassword(text) }} secureTextEntry={true} placeholder='Password' />
+                    <TextInput ref={confirmpassRef} style={[{ flex: 1, margin: 5 }, bootstrap.input, bootstrap.bg_palepurple]} onChangeText={(text) => { setconfirmpassword(text) }} placeholder='Confirm Password' />
+                </View>
+                <TouchableOpacity style={bootstrap.btn_darkpurple} onPress={signupfunc}>
+                    <Text style={[bootstrap.text_white, bootstrap.text_semibold]}>Create Account </Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
